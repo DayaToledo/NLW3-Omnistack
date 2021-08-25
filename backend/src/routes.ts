e566d776cb8f9a78d0bb4b28f4084aa3fbd3266c
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-08-24 22:24:23 -0300

    feat: refactor in controllers and routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from 'express';
+
+import OrphanagesController from './controllers/orphanagesController';
+
+const routes = Router();
+
+routes.post('/orphanages', OrphanagesController.create);
+
+export default routes;
